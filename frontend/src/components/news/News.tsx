a805a6ad9c209ae624e5e493517e9b3b79932850
sachin <{ID}+{username}@users.noreply.github.com>
2024-09-17 22:27:29 +0200

    add space_walk file

--- frontend/src/components/news/News.tsx
+++ frontend/src/components/news/News.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-// Assuming newsData is imported or defined somewhere in your component
-const newsData = [
-  { id: 1, title: 'Article 1', content: 'Content of article 1' },
-  { id: 2, title: 'Article 2', content: 'Content of article 2' },
-  // Add more articles as needed
-];
-//import * as newsData from './newsData.json'; 
+interface SpaceWalks {
+  id: number;
+  column1: string;
+  column2: number;
+  column3: string;
+  // add other properties if they exist
+}
 
-const News: React.FC = () => {
-  return (
+const News: React.FC = () => {  
+  const [newsData, setNewsData] = useState<SpaceWalks[]>([]);
+  useEffect(() => {
+    const fetchNewsData = async () => {
+      const result = await axios.get<SpaceWalks[]>('http://gaganyatri-django-spaces.hf.space/space_walks/api/space_walks/');
+      setNewsData(result.data);
+    };
+
+    fetchNewsData();
+  }, []);
+
+   return (
     <div>
-      <div className="container">
-        <div className="news-list">
-          {newsData.map((article) => (
-            <div key={article.id} className="news-item">
-              <h2 className="news-title">{article.title}</h2>
-              <p className="news-content">{article.content}</p>
-            </div>
-          ))}
+      {newsData.map((spaceWalk) => (
+        <div key={spaceWalk.id} className="news-item">
+          <h2 className="news-title">{spaceWalk.column1}</h2>
+          <p className="news-content">{spaceWalk.column2}</p>
+          <p className="news-date">{spaceWalk.column3}</p>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
