e8751095639502d65a0581c836fecbeefd563e9d
sachin <>
2024-09-18 21:29:35 +0200

    load data

--- backend/space_walks/models.py
+++ backend/space_walks/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 class SpaceWalks(models.Model):
     astronaut = models.CharField(max_length=100)
+    date = models.DateField()
     duration = models.DurationField()
-    date = models.DateTimeField(auto_now_add=True)
-    description = models.CharField(max_length=255)
+    description = models.TextField()
 
     def __str__(self):
-        return self.astronaut
+        return f"{self.astronaut} - {self.date}"
+
+    class Meta:
+        ordering = ['-date']
