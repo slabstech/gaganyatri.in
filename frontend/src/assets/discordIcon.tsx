a79e4446c81904318cd1405db25c1a2e6accac9a
sachin <>
2024-10-14 08:47:17 +0530

    update discord url

---
+++ frontend/src/assets/discordIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SvgIcon from '@mui/material/SvgIcon';
+
+const DiscordIcon = (props:any) => (
+    <SvgIcon {...props}>
+        <path d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02..."/>
+    </SvgIcon>
+);
+
+export default DiscordIcon;
