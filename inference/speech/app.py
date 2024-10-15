dd468fc8e5b336e27613ffc49b299a8ab142044d
sachin <>
2024-10-15 14:42:28 +0530

    faster-whisper gpu

--- inference/speech/app.py
+++ inference/speech/app.py
@@ -1,18 +1,13 @@
 from openai import OpenAI
 
-
-print('1')
 client = OpenAI(api_key="cant-be-empty", base_url="http://localhost:8000/v1/")
 
-print('2')
-audio_file = open("audio.wav", "rb")
+filename= '/home/gaganyatri/Music/test1.flac'
 
-print('3')
+audio_file = open(filename, "rb")
+
 transcript = client.audio.transcriptions.create(
     model="Systran/faster-distil-whisper-small.en", file=audio_file
 )
 
-print('3')
 print(transcript.text)
-
-print('4')
