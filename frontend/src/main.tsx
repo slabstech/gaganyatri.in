dfa95e8487c44b70826e01c3acd3d5501d416ec3
sachin <{ID}+{username}@users.noreply.github.com>
2024-11-25 00:33:11 +0100

    draft-add dashboard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,9 @@ import { Provider } from 'react-redux';
 import { store } from './reducers/store';
 import { BrowserRouter } from 'react-router-dom';
 
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
@@ -22,14 +25,25 @@ import Testimonials from './components/theme/marketing-page/components/Testimoni
 import FAQ from './components/theme/marketing-page/components/FAQ';
 import Footer from './components/theme/marketing-page/components/Footer';
 import AppTheme from './components/theme/shared-theme/AppTheme';
+import AppNavbar from './components/theme/dashboard/components/AppNavbar';
+import Header from './components/theme/dashboard/components/Header';
+import MainGrid from './components/theme/dashboard/components/MainGrid';
+import SideMenu from './components/theme/dashboard/components/SideMenu';
 
-// Create a theme instance.
-const theme = createTheme({
-  palette: {
-    primary: lightBlue,
-    secondary: deepOrange,
-  },
-});
+import {
+  chartsCustomizations,
+  dataGridCustomizations,
+  datePickersCustomizations,
+  treeViewCustomizations,
+} from './components/theme/dashboard/theme/customizations';
+
+const xThemeComponents = {
+  ...chartsCustomizations,
+  ...dataGridCustomizations,
+  ...datePickersCustomizations,
+  ...treeViewCustomizations,
+};
+
 /*
 const initialState = {
 
@@ -37,12 +51,39 @@ const initialState = {
 };
 <Root initialState={initialState}>
 */
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
 
-    <AppTheme >
+    <AppTheme themeComponents={xThemeComponents} >
       <CssBaseline enableColorScheme />
+      <Box sx={{ display: 'flex' }}>
+      <SideMenu />
+        <AppNavbar />
+        <Box
+          component="main"
+          sx={(theme) => ({
+            flexGrow: 1,
+            backgroundColor: theme.vars
+              ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
+              : alpha(theme.palette.background.default, 1),
+            overflow: 'auto',
+          })}
+        >
+          <Stack
+            spacing={2}
+            sx={{
+              alignItems: 'center',
+              mx: 3,
+              pb: 5,
+              mt: { xs: 8, md: 0 },
+            }}
+          >
+            <Header />
+            <MainGrid />
+          </Stack>
+        </Box>
 
       <AppAppBar />
       <Hero />
@@ -60,6 +101,7 @@ createRoot(document.getElementById('root')!).render(
         <Divider />
         <Footer />
       </div>
+      </Box>
     </AppTheme>
     </ErrorBoundary>
   </StrictMode>,
